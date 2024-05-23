1148e0f20e6f57547ea504ca33f53a6e7d9b018f
Rohitwagh812 <rohitwagh480@gmail.com>
2024-05-23 23:52:40 +0530

    first commit

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true 
 }));
 
 
